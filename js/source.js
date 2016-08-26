6e9d4f346eff87f4c889fdcd4a329583b51bd5d5
jgreenlee24 <jgreenlee@harding.edu>
2016-08-25 21:31:23 -0500

    Testing: Enabling JavaScript files; Disabling ajax calls

--- js/source.js
+++ js/source.js
@@ -15,15 +15,15 @@ $(document).ready(function() {
     $('code').hide();
     $('.html').show();
     $('.html .code-content').show();
-
-    //getting code for header code component
-    $.each(files, function(index, file) {
-        $.get(file, function(response) {
-            var doctype = file.substr(1, file.length).split('.').pop();
-            $('.' + doctype + ' .code-content').text(response);
-        });
-    });
-
+    //
+    // //getting code for header code component
+    // $.each(files, function(index, file) {
+    //     $.get(file, function(response) {
+    //         var doctype = file.substr(1, file.length).split('.').pop();
+    //         $('.' + doctype + ' .code-content').text(response);
+    //     });
+    // });
+    //
     //adds jQuery.on() for show and hide() events
     $.each(['show', 'hide'], function(i, ev) {
         var el = $.fn[ev];
@@ -39,36 +39,36 @@ $(document).ready(function() {
     });
 
     //scroll events
-    // $(window).scroll(function() {
-    //     // enable only after about section has been reached
-    //     if ($(this).scrollTop() >= $('#about').position().top) {
-    //         $('#fixed-top').show();
-    //         $('.fixed-left-button').show();
-    //         setTimeout(function() {
-    //             $('#fixed-top').hide();
-    //         }, 5000);
-    //     } else {
-    //         $('fixed-top').hide();
-    //         $('.fixed-left-button').hide();
-    //     }
-    //     /* Check the location of each desired element */
-    //     $('.hideme').each( function(i) {
-    //
-    //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //
-    //         /* If the object is completely visible in the window, fade it it */
-    //         if( bottom_of_window > bottom_of_object ){
-    //             $(this).animate({'opacity':'1'},500);
-    //         }
-    //     });
-    // });
+    $(window).scroll(function() {
+        // enable only after about section has been reached
+        if ($(this).scrollTop() >= $('#about').position().top) {
+            $('#fixed-top').show();
+            $('.fixed-left-button').show();
+            setTimeout(function() {
+                $('#fixed-top').hide();
+            }, 5000);
+        } else {
+            $('fixed-top').hide();
+            $('.fixed-left-button').hide();
+        }
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
 
     // highlight code
     hljs.initHighlightingOnLoad();
 
     // start code switch animation
-    // repeat();
+    repeat();
 });
 
 var repeat = function() {
