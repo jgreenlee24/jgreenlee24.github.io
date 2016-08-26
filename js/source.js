50bdb25889fe6c9d08b0d544467c8aafe604714c
jgreenlee24 <jgreenlee@harding.edu>
2016-08-25 20:39:20 -0500

    Testing: removing most JavaScript to see why page is slow.

--- js/source.js
+++ js/source.js
@@ -16,53 +16,53 @@ $(document).ready(function() {
     $('.html').show();
     $('.html .code-content').show();
 
-    //getting code for header code component
-    $.each(files, function(index, file) {
-        $.get(file, function(response) {
-            var doctype = file.substr(1, file.length).split('.').pop();
-            $('.' + doctype + ' .code-content').text(response);
-        });
-    });
-
-    //adds jQuery.on() for show and hide() events
-    $.each(['show', 'hide'], function(i, ev) {
-        var el = $.fn[ev];
-        $.fn[ev] = function() {
-            this.trigger(ev);
-            return el.apply(this, arguments);
-        };
-    });
-
-    //click events
-    $('.menu-square').click(function() {
-        $('#fixed-left').fadeToggle('slow');
-    });
-
-    //scroll events
-    $(window).scroll(function() {
-        // enable only after about section has been reached
-        if ($(this).scrollTop() >= $('#about').position().top) {
-            $('#fixed-top').show();
-            $('.fixed-left-button').show();
-            setTimeout(function() {
-                $('#fixed-top').hide();
-            }, 5000);
-        } else {
-            $('fixed-top').hide();
-            $('.fixed-left-button').hide();
-        }
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i) {
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},500);
-            }
-        });
-    });
+    // //getting code for header code component
+    // $.each(files, function(index, file) {
+    //     $.get(file, function(response) {
+    //         var doctype = file.substr(1, file.length).split('.').pop();
+    //         $('.' + doctype + ' .code-content').text(response);
+    //     });
+    // });
+    //
+    // //adds jQuery.on() for show and hide() events
+    // $.each(['show', 'hide'], function(i, ev) {
+    //     var el = $.fn[ev];
+    //     $.fn[ev] = function() {
+    //         this.trigger(ev);
+    //         return el.apply(this, arguments);
+    //     };
+    // });
+    //
+    // //click events
+    // $('.menu-square').click(function() {
+    //     $('#fixed-left').fadeToggle('slow');
+    // });
+    //
+    // //scroll events
+    // $(window).scroll(function() {
+    //     // enable only after about section has been reached
+    //     if ($(this).scrollTop() >= $('#about').position().top) {
+    //         $('#fixed-top').show();
+    //         $('.fixed-left-button').show();
+    //         setTimeout(function() {
+    //             $('#fixed-top').hide();
+    //         }, 5000);
+    //     } else {
+    //         $('fixed-top').hide();
+    //         $('.fixed-left-button').hide();
+    //     }
+    //     /* Check the location of each desired element */
+    //     $('.hideme').each( function(i) {
+    //
+    //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //
+    //         /* If the object is completely visible in the window, fade it it */
+    //         if( bottom_of_window > bottom_of_object ){
+    //             $(this).animate({'opacity':'1'},500);
+    //         }
+    //     });
+    // });
 
     // highlight code
     hljs.initHighlightingOnLoad();
