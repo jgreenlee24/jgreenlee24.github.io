74943e9670b1d769e369eb757016b7ed73969a6d
Justin B. Greenlee <justin.greenlee@rewardstyle.com>
2020-11-10 09:32:48 -0600

    added vue scaffolding

---
+++ src/router/routes/admin.js
@@ -0,0 +1,13 @@
+export default [
+  // {
+  //   path: '/admin/users',
+  //   name: 'admin-users',
+  //   meta: {
+  //     requiresAuth: true
+  //   },
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "admin-users" */ '@/components/AdminUsers.vue'
+  //     )
+  // }
+]
